2c21ec9c10fcaaa99ca46eb9931d1ea72a2af303
Angel Velasquez <334320@nttdata.com>
2024-08-08 01:58:19 -0600

    Initial commit

---
+++ main.tf
@@ -0,0 +1,382 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+locals {
+  principals_readonly_access_non_empty     = length(var.principals_readonly_access) > 0
+  principals_pull_through_access_non_empty = length(var.principals_pull_though_access) > 0
+  principals_push_access_non_empty         = length(var.principals_push_access) > 0
+  principals_full_access_non_empty         = length(var.principals_full_access) > 0
+  principals_lambda_non_empty              = length(var.principals_lambda) > 0
+  organizations_readonly_access_non_empty  = length(var.organizations_readonly_access) > 0
+  organizations_full_access_non_empty      = length(var.organizations_full_access) > 0
+  organizations_push_non_empty             = length(var.organizations_push_access) > 0
+
+  ecr_need_policy = (
+    length(var.principals_full_access)
+    + length(var.principals_readonly_access)
+    + length(var.principals_pull_though_access)
+    + length(var.principals_push_access)
+    + length(var.principals_lambda)
+    + length(var.organizations_readonly_access)
+    + length(var.organizations_full_access)
+    + length(var.organizations_push_access) > 0
+  )
+}
+
+resource "aws_ecr_repository" "name" {
+  for_each             = toset(var.image_names)
+  name                 = each.value
+  image_tag_mutability = var.image_tag_mutability
+  force_delete         = var.force_delete
+
+  dynamic "encryption_configuration" {
+    for_each = var.encryption_configuration == null ? [] : [var.encryption_configuration]
+    content {
+      encryption_type = encryption_configuration.value.encryption_type
+      kms_key         = encryption_configuration.value.kms_key
+    }
+  }
+
+  image_scanning_configuration {
+    scan_on_push = var.scan_images_on_push
+  }
+
+  tags = var.tags
+}
+
+
+locals {
+  untagged_image_rule = [
+    {
+      rulePriority = length(var.protected_tags) + 1
+      description  = "Remove untagged images"
+      selection = {
+        tagStatus   = "untagged"
+        countType   = "imageCountMoreThan"
+        countNumber = 1
+      }
+      action = {
+        type = "expire"
+      }
+    }
+  ]
+
+  remove_old_image_rule = [
+    {
+      rulePriority = length(var.protected_tags) + 2
+      description = (
+        var.time_based_rotation ?
+        "Rotate images older than ${var.max_image_count} days old" :
+        "Rotate images when reach ${var.max_image_count} images stored"
+      )
+      selection = merge(
+        {
+          tagStatus = "any"
+          countType = (
+            var.time_based_rotation ?
+            "sinceImagePushed" :
+            "imageCountMoreThan"
+          )
+          countNumber = var.max_image_count
+        },
+        var.time_based_rotation ? { countUnit = "days" } : {}
+      )
+      action = {
+        type = "expire"
+      }
+    }
+  ]
+
+  protected_tag_rules = [
+    for index, tagPrefix in zipmap(range(length(var.protected_tags)), tolist(var.protected_tags)) :
+    {
+      rulePriority = tonumber(index) + 1
+      description  = "Protects images tagged with ${tagPrefix}"
+      selection = {
+        tagStatus     = "tagged"
+        tagPrefixList = [tagPrefix]
+        countType     = "imageCountMoreThan"
+        countNumber   = 999999
+      }
+      action = {
+        type = "expire"
+      }
+    }
+  ]
+}
+
+resource "aws_ecr_lifecycle_policy" "name" {
+  for_each   = toset(var.enable_lifecycle_policy ? var.image_names : [])
+  repository = aws_ecr_repository.name[each.value].name
+
+  policy = jsonencode({
+    rules = concat(local.protected_tag_rules, local.untagged_image_rule, local.remove_old_image_rule)
+  })
+}
+
+data "aws_iam_policy_document" "empty" {
+}
+
+data "aws_partition" "current" {}
+
+data "aws_iam_policy_document" "resource_readonly_access" {
+  statement {
+    sid    = "ReadonlyAccess"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = var.principals_readonly_access
+    }
+
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      "ecr:DescribeImageScanFindings",
+      "ecr:DescribeImages",
+      "ecr:DescribeRepositories",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetLifecyclePolicy",
+      "ecr:GetLifecyclePolicyPreview",
+      "ecr:GetRepositoryPolicy",
+      "ecr:ListImages",
+      "ecr:ListTagsForResource",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "resource_pull_through_cache" {
+  statement {
+    sid    = "PullThroughAccess"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = var.principals_pull_though_access
+    }
+
+    actions = [
+      "ecr:BatchImportUpstreamImage",
+      "ecr:TagResource"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "resource_push_access" {
+  statement {
+    sid    = "PushAccess"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = var.principals_push_access
+    }
+
+    actions = [
+      "ecr:CompleteLayerUpload",
+      "ecr:GetAuthorizationToken",
+      "ecr:UploadLayerPart",
+      "ecr:InitiateLayerUpload",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "resource_full_access" {
+  statement {
+    sid    = "FullAccess"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = var.principals_full_access
+    }
+
+    actions = ["ecr:*"]
+  }
+}
+
+data "aws_iam_policy_document" "lambda_access" {
+  count = length(var.principals_lambda) > 0 ? 1 : 0
+
+  statement {
+    sid    = "LambdaECRImageCrossAccountRetrievalPolicy"
+    effect = "Allow"
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:GetDownloadUrlForLayer"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      values   = local.principals_lambda_non_empty ? formatlist("arn:%s:lambda:*:%s:function:*", data.aws_partition.current.partition, var.principals_lambda) : []
+      variable = "aws:SourceArn"
+    }
+  }
+
+  statement {
+    sid    = "CrossAccountPermission"
+    effect = "Allow"
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:GetDownloadUrlForLayer"
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = local.principals_lambda_non_empty ? formatlist("arn:%s:iam::%s:root", data.aws_partition.current.partition, var.principals_lambda) : []
+    }
+  }
+}
+
+data "aws_iam_policy_document" "organizations_readonly_access" {
+  count = length(var.organizations_readonly_access) > 0 ? 1 : 0
+
+  statement {
+    sid    = "OrganizationsReadonlyAccess"
+    effect = "Allow"
+
+    principals {
+      identifiers = ["*"]
+      type        = "*"
+    }
+
+    condition {
+      test     = "StringEquals"
+      values   = var.organizations_readonly_access
+      variable = "aws:PrincipalOrgID"
+    }
+
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      "ecr:DescribeImageScanFindings",
+      "ecr:DescribeImages",
+      "ecr:DescribeRepositories",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetLifecyclePolicy",
+      "ecr:GetLifecyclePolicyPreview",
+      "ecr:GetRepositoryPolicy",
+      "ecr:ListImages",
+      "ecr:ListTagsForResource",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "organization_full_access" {
+  count = length(var.organizations_full_access) > 0 ? 1 : 0
+
+  statement {
+    sid    = "OrganizationsFullAccess"
+    effect = "Allow"
+
+    principals {
+      identifiers = ["*"]
+      type        = "*"
+    }
+
+    condition {
+      test     = "StringEquals"
+      values   = var.organizations_full_access
+      variable = "aws:PrincipalOrgID"
+    }
+
+    actions = [
+      "ecr:*",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "organization_push_access" {
+  count = length(var.organizations_push_access) > 0 ? 1 : 0
+
+  statement {
+    sid    = "OrganizationsPushAccess"
+    effect = "Allow"
+
+    principals {
+      identifiers = ["*"]
+      type        = "*"
+    }
+
+    condition {
+      test     = "StringEquals"
+      values   = var.organizations_push_access
+      variable = "aws:PrincipalOrgID"
+    }
+
+    actions = [
+      "ecr:CompleteLayerUpload",
+      "ecr:GetAuthorizationToken",
+      "ecr:UploadLayerPart",
+      "ecr:InitiateLayerUpload",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "resource" {
+  for_each = toset(local.ecr_need_policy ? var.image_names : [])
+  source_policy_documents = local.principals_readonly_access_non_empty ? [
+    data.aws_iam_policy_document.resource_readonly_access[0].json
+  ] : [data.aws_iam_policy_document.empty[0].json]
+  override_policy_documents = distinct([
+    local.principals_pull_through_access_non_empty && contains(var.prefixes_pull_through_repositories, regex("^[a-z][a-z0-9\\-\\.\\_]+", each.value)) ? data.aws_iam_policy_document.resource_pull_through_cache[0].json : data.aws_iam_policy_document.empty[0].json,
+    local.principals_push_access_non_empty ? data.aws_iam_policy_document.resource_push_access[0].json : data.aws_iam_policy_document.empty[0].json,
+    local.principals_full_access_non_empty ? data.aws_iam_policy_document.resource_full_access[0].json : data.aws_iam_policy_document.empty[0].json,
+    local.principals_lambda_non_empty ? data.aws_iam_policy_document.lambda_access[0].json : data.aws_iam_policy_document.empty[0].json,
+    local.organizations_full_access_non_empty ? data.aws_iam_policy_document.organization_full_access[0].json : data.aws_iam_policy_document.empty[0].json,
+    local.organizations_readonly_access_non_empty ? data.aws_iam_policy_document.organizations_readonly_access[0].json : data.aws_iam_policy_document.empty[0].json,
+    local.organizations_push_non_empty ? data.aws_iam_policy_document.organization_push_access[0].json : data.aws_iam_policy_document.empty[0].json
+  ])
+}
+
+resource "aws_ecr_repository_policy" "name" {
+  for_each   = toset(local.ecr_need_policy ? var.image_names : [])
+  repository = aws_ecr_repository.name[each.value].name
+  policy     = data.aws_iam_policy_document.resource[each.value].json
+}
+
+resource "aws_ecr_replication_configuration" "replication_configuration" {
+  count = length(var.replication_configurations) > 0 ? 1 : 0
+  dynamic "replication_configuration" {
+    for_each = var.replication_configurations
+    content {
+      dynamic "rule" {
+        for_each = replication_configuration.value.rules
+        content {
+          dynamic "destination" {
+            for_each = rule.value.destinations
+            content {
+              region      = destination.value.region
+              registry_id = destination.value.registry_id
+            }
+          }
+          dynamic "repository_filter" {
+            for_each = rule.value.repository_filters
+            content {
+              filter      = repository_filter.value.filter
+              filter_type = repository_filter.value.filter_type
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,146 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+variable "image_names" {
+  type        = list(string)
+  default     = []
+  description = "List of Docker local image names, used as repository names for AWS ECR "
+}
+
+variable "image_tag_mutability" {
+  type        = string
+  default     = "INMUTABLE"
+  description = "The tag mutability setting for the repository. Must be one of: MUTABLE or IMMUTABLE"
+}
+
+variable "force_delete" {
+  type        = bool
+  default     = false
+  description = "If true, the repository will be deleted even if it contains images"
+}
+
+variable "encryption_configuration" {
+  type = object({
+    encryption_type = string
+    kms_key         = string
+  })
+  default     = null
+  description = "The encryption configuration for the repository. If not set, the repository will not be encrypted"
+}
+
+variable "scan_images_on_push" {
+  type        = bool
+  default     = true
+  description = "If true, images will be scanned for vulnerabilities after being pushed to the repository"
+}
+
+variable "protected_tags" {
+  type        = list(string)
+  default     = []
+  description = "List of image tag prefixes which are protected. If a tag is created with a prefix that is in this list, it will be protected"
+}
+
+variable "time_based_rotation" {
+  type        = bool
+  default     = false
+  description = "The time-based image rotation configuration for the repository. If not set, the repository will not have a time-based image rotation configuration"
+}
+
+variable "max_image_count" {
+  type        = number
+  default     = 500
+  description = "The maximum number of images to retain in the repository. If not set, the repository will retain 500 images"
+}
+
+variable "enable_lifecycle_policy" {
+  type        = bool
+  default     = true
+  description = "If true, the repository will have a lifecycle policy"
+}
+
+variable "principals_readonly_access" {
+  type        = list(string)
+  default     = []
+  description = "List of AWS account IDs or IAM roles with read-only access to the repository"
+}
+
+variable "principals_pull_though_access" {
+  type        = list(string)
+  default     = []
+  description = "List of AWS account IDs or IAM roles with pull-through access to the repository"
+}
+
+variable "principals_push_access" {
+  type        = list(string)
+  default     = []
+  description = "List of AWS account IDs or IAM roles with push access to the repository"  
+}
+
+variable "principals_full_access" {
+  type        = list(string)
+  default     = []
+  description = "List of AWS account IDs or IAM roles with full access to the repository"  
+}
+
+variable "principals_lambda" {
+  type        = list(string)
+  default     = []
+  description = "List of AWS account IDs or IAM roles with lambda access to the repository"  
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "A map of tags to assign to the resource"
+}
+
+variable "organizations_readonly_access" {
+  type        = list(string)
+  default     = []
+  description = "List of AWS Organizations IDs with read-only access to the repository"  
+}
+
+variable "organizations_full_access" {
+  type        = list(string)
+  default     = []
+  description = "List of AWS Organizations IDs with full access to the repository"    
+}
+
+variable "organizations_push_access" {
+  type        = list(string)
+  default     = []
+  description = "List of AWS Organizations IDs with push access to the repository"      
+}
+
+variable "prefixes_pull_through_repositories" {
+  type        = list(string)
+  default     = []
+  description = "List of repository prefixes that will have pull-through access to the repository"      
+}
+
+variable "replication_configurations" {
+  type = list(object({
+    rules = list(object({          # Maximum 10
+      destinations = list(object({ # Maximum 25
+        region      = string
+        registry_id = string
+      }))
+      repository_filters = list(object({
+        filter      = string
+        filter_type = string
+      }))
+    }))
+  }))
+  default = []
+  description = "List of replication configurations for the repository"
+  
+}
