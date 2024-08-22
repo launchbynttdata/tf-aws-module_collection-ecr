d6527ceca304605b17cc4f1a1a1818771d15875b
Angel Velasquez <Angel.Velasquez@nttdata.com>
2024-08-22 16:36:26 -0600

    Update tests/post_deploy_functional_readonly/main_test.go
    Co-authored-by: Bryce Lynn <160785634+bryce-lynn-nttd@users.noreply.github.com>
    Signed-off-by: Angel Velasquez <Angel.Velasquez@nttdata.com>

--- tests/post_deploy_functional_readonly/main_test.go
+++ tests/post_deploy_functional_readonly/main_test.go
@@ -33,5 +33,5 @@ func TestCollectionEcrModule(t *testing.T) {
 		SetTestConfigFileName(infraTFVarFileNameDefault).
 		Build()
 
-	lib.RunSetupTestTeardown(t, *ctx, testimpl.TestEcrCollection)
+	lib.RunNonDestructiveTest(t, *ctx, testimpl.TestEcrCollection)
 }
