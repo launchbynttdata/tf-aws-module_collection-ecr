9d5fba566381eb1883e2815daa1c3ee4b60e0ac3
aarti.joshi <aarti.joshi@nttdata.com>
2025-11-07 14:25:03 -0500

    fix

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@
 
 module "ecr" {
   source                             = "cloudposse/ecr/aws"
-  version                            = "~> 1.0.0"
+  version                            = "~> 0.41"
   additional_tag_map                 = var.additional_tag_map
   attributes                         = var.attributes
   context                            = var.context
